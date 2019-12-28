2541229c577716d7771baa8c0219d16830896e25
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-12-27 21:32:24 -0800

    test(vector2): add vector2 class method tests
    Add toString and length method tests.

--- tests/vector2.spec.ts
+++ tests/vector2.spec.ts
@@ -1,10 +1,26 @@
 import { default as Vector2 } from "../src/vector2";
 
 describe("Vector2", () => {
-    it("should initialize a new Vector2", () => {
+    it("initializes a new Vector2", () => {
         const v = new Vector2();
 
         expect(v.x).toBe(0);
         expect(v.y).toBe(0);
+    });
+
+    it("toString() method", () => {
+        const v = new Vector2();
+        const string = v.toString();
+
+        expect(typeof string).toBe("string");
+        expect(string).toBe("x: 0, y: 0");
+    });
+
+    it("length() method", () => {
+        const v = new Vector2(15, 20);
+        const length = v.length();
+
+        expect(typeof length).toBe("number");
+        expect(length).toBe(35);
     });
 });
