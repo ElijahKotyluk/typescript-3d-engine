c8bf97a632d5f744643dd6e93cb6496f36438eb7
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-12-28 22:53:30 -0800

    chore(vector2): add methods
    Add more class methods.

--- src/vector2.ts
+++ src/vector2.ts
@@ -7,7 +7,7 @@ export default class Vector2 {
     x: number;
     y: number;
 
-    constructor(x:number, y: number) {
+    constructor(x?:number, y?: number) {
         this.x = x || 0;
         this.y = y || 0;
     }
@@ -26,5 +26,54 @@ export default class Vector2 {
 
     public clone(): Vector2 {
         return new Vector2(this.x, this.y);
+    }
+
+    public copy(vector: Vector2): Vector2 {
+        this.x = vector.x;
+        this.y = vector.y;
+
+        return this;
+    }
+
+    public add(vector: Vector2): Vector2 {
+        this.x += vector.x;
+        this.y += vector.y;
+
+        return this;
+    }
+
+    public subtract(vector: Vector2): Vector2 {
+        this.x -= vector.x;
+        this.y -= vector.y;
+
+        return this;
+    }
+
+    public multiply(vector: Vector2): Vector2 {
+        this.x *= vector.x;
+        this.y *= vector.y;
+
+        return this;
+    }
+
+    public divide(vector: Vector2): Vector2 {
+        this.x /= vector.x;
+        this.y /= vector.y;
+
+        return this;
+    }
+
+    public negate(): Vector2 {
+        this.x = -this.x;
+        this.y = -this.y;
+
+        return this;
+    }
+
+    public zero(): Vector2 {
+        this.x = 0;
+        this.y = 0;
+
+        return this;
     }
 }
